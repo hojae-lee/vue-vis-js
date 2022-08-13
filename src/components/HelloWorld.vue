93ebba606eaea1d28f2c4b191299604c5faf7bee
jaehoLee <chosloo2835@gmail.com>
2022-08-13 23:13:06 +0900

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,7 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="">
+    <h2>{{msg}}</h2>
+    <vis-network class="network" ref="network" :nodes="nodes" :edges="edges" :options="options"></vis-network>
   </div>
 </template>
 
@@ -35,24 +10,56 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      nodes: [
+      {id: 2696, label: "2696", hidden: false},
+      {id: 1205, label: "1205", hidden: false},
+      {id: 2697, label: "2697", hidden: false},
+      {id: 2021, label: "2021", hidden: true},
+      {id: 2047, label: "2047", hidden: true},
+      {id: 1696, label: "2696", hidden: false},
+    ],
+      edges: [
+      {id: 'e1', from: 2696, to: 2697, hidden: false},
+      {id: 'e2', from: 2696, to: 1205, hidden: false},
+      {id: 'e3', from: 1205, to: 2021, hidden: true},
+      {id: 'e4', from: 1205, to: 2047, hidden: true}
+    ],
+      options: {
+        nodes: {
+          shape: "dot"
+        },
+        edges: {
+          width: 2,
+          smooth: { type: "curvedCW", forceDirection: "none", roundness: 0.2 }
+        },
+        physics: {
+          enabled: false,
+          barnesHut: {
+            theta: 0.5,
+            // gravitationalConstant: gravitationalConstant,
+            centralGravity: 0,
+            springLength: 10,
+            springConstant: 0.04,
+            damping: 0.09,
+            avoidOverlap: 1
+          }
+        },
+        layout: {
+          randomSeed: 3
+        }
+      }
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.network {
+  width: 600px;
+  height: 600px;
+  border: 1px solid lightgray;
 }
 </style>
