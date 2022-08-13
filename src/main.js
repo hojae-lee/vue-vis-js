baa7c8c368230be6add7e407862717b4b249cc4e
jaehoLee <chosloo2835@gmail.com>
2022-08-13 23:14:59 +0900

    update

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { Network } from 'vue-vis-network';
 
 Vue.config.productionTip = false
+Vue.component('visNetwork', Network);
 
 new Vue({
   render: h => h(App),
