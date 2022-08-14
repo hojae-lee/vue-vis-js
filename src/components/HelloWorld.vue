0693106c99c3812639b66d52d9fa2f583bfbf7a2
jaehoLee <chosloo2835@gmail.com>
2022-08-14 23:01:12 +0900

    add vue-network

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,32 +14,35 @@ export default {
   data() {
     return {
       nodes: [
-      {id: 2696, label: "2696", hidden: false},
-      {id: 1205, label: "1205", hidden: false},
-      {id: 2697, label: "2697", hidden: false},
-      {id: 2021, label: "2021", hidden: true},
-      {id: 2047, label: "2047", hidden: true},
-      {id: 1696, label: "2696", hidden: false},
-    ],
+        {id: "ap1", label: "ap1", hidden: false, shape: "image", image: "./logo.png"},
+        {id: "pro1", label: "pro1", hidden: false},
+        {id: "pro2", label: "pro2", hidden: false},
+        {id: "pro3", label: "pro3", hidden: false},
+        {id: "pro4", label: "pro4", hidden: false, color: {border: "#666", highlight: {border:"#666"}}},
+      ],
       edges: [
-      {id: 'e1', from: 2696, to: 2697, hidden: false},
-      {id: 'e2', from: 2696, to: 1205, hidden: false},
-      {id: 'e3', from: 1205, to: 2021, hidden: true},
-      {id: 'e4', from: 1205, to: 2047, hidden: true}
-    ],
+        { id: 'e1', from: "ap1", to: "pro1", label: "33" },
+        { id: 'e2', from: "ap1", to: "pro2", label: "11", dashes: true },
+        { id: 'e3', from: "ap1", to: "pro3", label: "134", background: { enabled: true, color: "#000", size: 5 }},
+        { id: 'e4', from: "ap1", to: "pro4", label: "", dashes: [1, 4], color: "#666" },
+      ],
       options: {
         nodes: {
-          shape: "dot"
-        },
-        edges: {
-          width: 2,
-          smooth: { type: "curvedCW", forceDirection: "none", roundness: 0.2 }
+          shape: "dot",
+          font: {
+            size: 15,
+            color: "#ffffff"
+          },
+          color: {
+            background: "#222222",
+            highlight: {
+              background: "#222222"
+            }
+          }
         },
         physics: {
           enabled: false,
           barnesHut: {
-            theta: 0.5,
-            // gravitationalConstant: gravitationalConstant,
             centralGravity: 0,
             springLength: 10,
             springConstant: 0.04,
@@ -57,9 +60,11 @@ export default {
 </script>
 
 <style scoped>
+
 .network {
-  width: 600px;
-  height: 600px;
-  border: 1px solid lightgray;
+  width: 800px;
+  height: 800px;
+  border: 1px solid #444444;
+  background-color: #222222;
 }
 </style>
